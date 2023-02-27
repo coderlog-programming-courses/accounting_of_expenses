730b7091518b7eedb4abcdb78f91f5f555338c73
Ілля Корпан <114827858+Calover@users.noreply.github.com>
2023-02-27 21:01:32 +0200

    Update db_interface.py

--- db_interface.py
+++ db_interface.py
@@ -1,15 +1,7 @@
 import sqlite3
 
-def insert_contact(name, username, password):
+def insert_contact(username, password):
     conn = sqlite3.connect('contact_information.db')
-    conn.execute('INSERT INTO CONTACT_INFORMATION (NAME, USERNAME, PASSWORD) VALUES(?, ?, ?)', (name, username, password))
+    conn.execute("INSERT INTO CONTACT_INFORMATION (USERNAME, PASSWORD) VALUES('%s', '%s')"%(username, password))
     conn.commit()
     conn.close()
-
-def retrieve_contacts():
-    results = []
-    conn = sqlite3.connect('contact_information.db')
-    cursor = conn.execute("SELECT name, username, password from CONTACT_INFORMATION")
-    for row in cursor:
-        result.append(list(row))
-    return results
